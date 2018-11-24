2afe62336e6f2a7c0bdadb5c8a4b82d3952bcacf
Kazuyuki Tanahasi <tana@cup.com>
2018-11-25 03:41:49 +0900

    画像の最適化

--- js/creative.js
+++ js/creative.js
@@ -74,3 +74,12 @@
   });
 
 })(jQuery); // End of use strict
+
+var ua = navigator.userAgent.toLowerCase();
+var isMobile = /iphone/.test(ua)||/android(.+)?mobile/.test(ua);
+
+if (!isMobile) {
+    $('a[href^="tel:"]').on('click', function(e) {
+        e.preventDefault();
+    });
+}
